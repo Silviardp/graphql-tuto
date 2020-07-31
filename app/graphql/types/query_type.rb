4412d5fbd394efd4650dcc96aa938895b369c559
Silvia Rocha <silviadprocha@gmail.com>
2020-07-31 16:51:42 +0200

    create search resolver

--- app/graphql/types/query_type.rb
+++ app/graphql/types/query_type.rb
@@ -1,9 +1,10 @@
 module Types
   class QueryType < Types::BaseObject
-   field :all_links, [LinkType], null: false
+   field :all_links, resolver: Resolvers::LinksSearch
 
    def all_links
       Link.all
    end
+
   end
 end
